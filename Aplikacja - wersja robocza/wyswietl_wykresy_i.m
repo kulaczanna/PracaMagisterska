f7fc8da8570b4a26af4bff062f35fe4ebb703dda
Anna Kułacz <43969613+kulaczanna@users.noreply.github.com>
2020-05-06 13:22:08 +0200

    model Isaeva Osipov metody leczenia - pojedyncze wykresy chyba działają ;p 6.05.2020

--- Aplikacja - wersja robocza/wyswietl_wykresy_i.m
+++ Aplikacja - wersja robocza/wyswietl_wykresy_i.m
@@ -1,61 +1,51 @@
-function [] = wyswietl_wykresy_i(t, y0, y1, y2, y3)
+function [] = wyswietl_wykresy_i(t, y, V_I)
       
-        figure
-        semilogy(t, y0(:,1), 'r');
-        hold on
-        semilogy(t, y1(:,1), 'g');
-        hold on
-        semilogy(t, y2(:,1), 'b');
-        hold on
-        semilogy(t, y3(:,1), 'k');
-        axis([0, 140, 10e4, 10e9])
-        legend('Brak leczenia', 'Chemioterapia', 'Immunoterapia IL-2', 'Immunoterapia IL-2 + IFN-alfa');
-        ylabel('Liczba komórek nowotworowych')
-        xlabel('Czas [dni]')
+%         figure
+%         semilogy(t, y(:,2), 'r');
+%         hold on
+%         semilogy(t, y(:,2), 'g');
+%         hold on
+%         semilogy(t, y(:,2), 'b');
+%         hold on
+%         semilogy(t, y(:,2), 'k');
+%         axis([0, 140, 10e4, 10e9])
+%         legend('Brak leczenia', 'Chemioterapia', 'Immunoterapia IL-2', 'Immunoterapia IL-2 + IFN-alfa');
+%         ylabel('Liczba komórek nowotworowych')
+%         xlabel('Czas [dni]')
         
+%         figure
+%         semilogy(t, y0(:,2), 'r');
+%         hold on
+%         semilogy(t, y1(:,2), 'g');
+%         hold on
+%         semilogy(t, y2(:,2), 'b');
+%         hold on
+%         semilogy(t, y3(:,2), 'k');
+%         axis([0, 140, 10e6, 10e10])
+%         legend('Brak leczenia', 'Chemioterapia', 'Immunoterapia IL-2', 'Immunoterapia IL-2 + IFN-alfa');
+%         ylabel('Liczba komórek CTL')
+%         xlabel('Czas [dni]')
+%         
         figure
-        semilogy(t, y0(:,2), 'r');
-        hold on
-        semilogy(t, y1(:,2), 'g');
-        hold on
-        semilogy(t, y2(:,2), 'b');
-        hold on
-        semilogy(t, y3(:,2), 'k');
-        axis([0, 140, 10e6, 10e10])
-        legend('Brak leczenia', 'Chemioterapia', 'Immunoterapia IL-2', 'Immunoterapia IL-2 + IFN-alfa');
-        ylabel('Liczba komórek CTL')
-        xlabel('Czas [dni]')
-        
-        figure
-        semilogy(t, y0(:,2), 'r');
-        hold on
-        semilogy(t, y1(:,2), 'g');
-        hold on
-        semilogy(t, y2(:,2), 'b');
-        hold on
-        semilogy(t, y3(:,2), 'k');
+        plot(t, y(:, 4)/1e6, 'r');
         axis([0, 140, 0, 60])
-        legend('Brak leczenia', 'Chemioterapia', 'Immunoterapia IL-2', 'Immunoterapia IL-2 + IFN-alfa');
-        ylabel('Liczba komórek IL-2')
+        legend('Chemioterapia');
+        ylabel('Liczba jednostek IL-2')
         xlabel('Czas [dni]')
-
+% 
         figure
-        plot(t, y0(:, 3), 'g');
+        plot(t, y(:, 3), 'g');
+        axis([0, 50, 0, 0.2])
         legend('Cytostatyk')
         ylabel('Stężenie')
         xlabel('Czas [dni]')
-        hold on
+        
 %         
-%         subplot(3, 1, 2)
-%         plot(t, y(:, 4), 'g');
-%         legend('IL-2')
-%         ylabel('Stężenie')
-%         xlabel('Czas [dni]')
-%         hold on
-%         
-%         subplot(3, 1, 3)
-%         plot(t, y(:, 5), 'g');
+%         figure
+%         plot(t, y0(:, 5), 'g');
 %         legend('IFN-alfa')
 %         ylabel('Stężenie')
 %         xlabel('Czas [dni]')
+%         hold on
+
 end
