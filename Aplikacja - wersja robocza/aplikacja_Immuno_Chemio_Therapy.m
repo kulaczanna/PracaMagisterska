cfbb082d1beff84d605793430fa7a1a9957f070f
Anna Kułacz <43969613+kulaczanna@users.noreply.github.com>
2020-05-18 07:09:43 +0200

    główna apka przerobiona, nie do końca śmiga 18.05.2020

--- Aplikacja - wersja robocza/aplikacja_Immuno_Chemio_Therapy.m
+++ Aplikacja - wersja robocza/aplikacja_Immuno_Chemio_Therapy.m
@@ -2,7 +2,7 @@ clc; clear all
 close all
 format long
 
-metoda_leczenia = 3;
+metoda_leczenia = 7;
 
 % Metody leczenia:
 % 1. brak leczenia - regresja nowotworu
@@ -20,7 +20,7 @@ pacjent = 9; % 9 lub 10 jak w artykule
 M_0 = 0; % stê¿enie cytostatyku M(t)
 liczba_dni_w_cyklu = 0; % co ile dni wystêpuje dodzowanie cytostatyku
 I_0 = 0; % stê¿enie IL-2 I(t)
-Ialfa_0 = 0; % stê¿enie IFN-alfa Ialfa(t)
+Ialfa_0 = 5; % stê¿enie IFN-alfa Ialfa(t)
         
 switch metoda_leczenia
    case 1 %ok
@@ -35,43 +35,38 @@ switch metoda_leczenia
         L_0 = 1e1;% CD8+T cells L(t)
         C_0 = 6e8;% cell C(t)
             
-    case 3 % chemioterapia, dawki co 5 dni, V_M = 5 - regresja nowotworu
-           % rys. 8 w artykule
-        T_0 = 1e6;
+    case 3 % chemioterapia, w miare ok
+        T_0 = 5e6;
         N_0 = 1e3;
         L_0 = 1e1;
         C_0 = 6e8;
         liczba_dni_w_cyklu = 5;
        
-    case 4  % chemioterapia, dawki co 10 dni, V_M = 5 - rozwój nowotworu
-            % rys. 8 w artykule
-        T_0 = 7e6;
+    case 4  % chemioterapia, w miare ok
+        T_0 = 3e7;
         N_0 = 1e3;
         L_0 = 1e1;
         C_0 = 6e8;
         liczba_dni_w_cyklu = 5;
         
     case 5  % immunoterapia
-            % rys. 9 w artykule
         T_0 = 1e6;
-        N_0 = 1e5;
-        L_0 = 1e2;
-        C_0 = 6e10;
+        N_0 = 1e3;
+        L_0 = 1e1;
+        C_0 = 6e8;
        
     case 6  % immunoterapia
-            % rys. 9 w artykule
-        T_0 = 2e7; %
-        N_0 = 3e5; % zmiana
-        L_0 = 1e2; %
-        C_0 = 10e10; %
-
-    case 7  % biochemioterapia
-            % rys. 10 w artykule
-        T_0 = 2e7; %
-        N_0 = 3e5; % zmiana
-        L_0 = 1e2; %
-        C_0 = 10e10; %
-        liczba_dni_w_cyklu = 10;
+        T_0 = 7e6;
+        N_0 = 1e3;
+        L_0 = 1e1;
+        C_0 = 6e8;
+       
+    case 7  % biochemioterapia niby ok ale nie
+        T_0 = 1e7; %
+        N_0 = 1e3; % zmiana
+        L_0 = 1e1; %
+        C_0 = 6e8; %
+        liczba_dni_w_cyklu = 5;
 end
 
 x = [T_0; N_0; L_0; C_0; M_0; I_0; Ialfa_0; liczba_dni_w_cyklu; pacjent; metoda_leczenia]; % parametry wejœciowe  uk³adu równañ
@@ -90,7 +85,7 @@ semilogy(t, y(:,4), 'c');
 hold on
 semilogy(t, y(:,5), 'k');
 hold on
-semilogy(t, y(:,6), 'b');
+semilogy(t, y(:,6), 'm');
 hold on
 semilogy(t, y(:,7), 'y');
 axis([0, 120, 10e-1, 10e12])
