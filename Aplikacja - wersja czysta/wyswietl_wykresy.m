6172a70986eaadd6dfd37725703bd53f726044f1
Anna Kułacz <43969613+kulaczanna@users.noreply.github.com>
2020-08-06 11:46:12 +0200

    końcówka v21 wykres bez podpisów chemioterapia s4, s5 6.08.2020

--- Aplikacja - wersja czysta/wyswietl_wykresy.m
+++ Aplikacja - wersja czysta/wyswietl_wykresy.m
@@ -11,7 +11,7 @@ semilogy(t, y(:,4), 'c');
 axis([0, 120, 10e-1, 10e12])
 % axis([0, 120, 0, 10e12])
 legend('Komórki nowotworowe', 'Komórki NK', 'Limfocyty T_{CD8+}', 'Kr¹¿¹ce limfocyty');
-title({'Zmiany w czasie liczby komórek badanych populacji';'dla wartoœci pocz¹tkowych:';'T(0) = 1,8^7, N(0) = 10^5, L(0) = 10^2, C(0) = 3,5*10^9'})
+title({'Zmiany w czasie liczby komórek badanych populacji';'dla wartoœci pocz¹tkowych:';'T(0) = 1,8*10^7, N(0) = 10^5, L(0) = 10^2, C(0) = 3,5*10^9'})
 ylabel('Liczba komórek [log]')
 xlabel('Czas [dni]')
 
