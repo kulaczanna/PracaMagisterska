a980d832770c9e11df1408e2de11e28aaf0abb11
Anna Kułacz <43969613+kulaczanna@users.noreply.github.com>
2020-08-12 02:49:08 +0200

    końcówka v22 wykresy podpisy regresja 12.08.2020

--- Aplikacja - wersja czysta/aplikacja_de_Pillis.m
+++ Aplikacja - wersja czysta/aplikacja_de_Pillis.m
@@ -13,7 +13,7 @@ metoda_leczenia = 7;
 % 6. immunoterapia - rozwój nowotworu
 % 7. immunoterapia + chemioterapia
 
-pacjent = 10; % 9 lub 10 jak w artykule
+pacjent = 9; % 9 lub 10 jak w artykule
 
 % pocz¹tkowe stê¿enie leków
 M_0 = 0; % stê¿enie cytostatyku M(t)
@@ -42,7 +42,7 @@ switch metoda_leczenia
         N_0 = 1e5;
         L_0 = 1e2;
         C_0 = 3.5e9;
-        liczba_dni_w_cyklu = 4;
+        liczba_dni_w_cyklu = 8;
         dni_dawkowania = 1;
        
     case 4  % chemioterapia, dawki co 10 dni, V_M = 5 - rozwój nowotworu
--- Aplikacja - wersja czysta/podaj_cytostatyk.m
+++ Aplikacja - wersja czysta/podaj_cytostatyk.m
@@ -1,23 +1,23 @@
 function V_M = podaj_cytostatyk(t, liczba_dni_w_cyklu, dawkowanie)
 
     if(t >= 0 + 0 && t <= dawkowanie + 0)
-        V_M = 0.5;
-%     elseif(t >= liczba_dni_w_cyklu + 0 && t <= liczba_dni_w_cyklu + dawkowanie + 0)
-%         V_M = 0.5;
-%     elseif(t >= 2*liczba_dni_w_cyklu + 0 && t <= (2*liczba_dni_w_cyklu) + dawkowanie + 0)
-%         V_M = 0.5;
-%     elseif(t >= 3*liczba_dni_w_cyklu + 0 && t <= (3*liczba_dni_w_cyklu) + dawkowanie + 0)
-%         V_M = 0.5;
-%     elseif(t >= 4*liczba_dni_w_cyklu + 0 && t <= (4*liczba_dni_w_cyklu) + dawkowanie + 0)
-%         V_M = 0.5;
-%     elseif(t >= 5*liczba_dni_w_cyklu + 0 && t <= (5*liczba_dni_w_cyklu) + dawkowanie + 0)
-%         V_M = 0.5;
-%     elseif(t >= 6*liczba_dni_w_cyklu + 0 && t <= (6*liczba_dni_w_cyklu) + dawkowanie + 0)
-%         V_M = 0.5;
-%     elseif(t >= 7*liczba_dni_w_cyklu + 0 && t <= (7*liczba_dni_w_cyklu) + dawkowanie + 0)
-%         V_M = 0.5;
-%     elseif(t >= 8*liczba_dni_w_cyklu + 0 && t <= (8*liczba_dni_w_cyklu) + dawkowanie + 0)
-%         V_M = 0.5;
+        V_M = 5;
+    elseif(t >= liczba_dni_w_cyklu + 0 && t <= liczba_dni_w_cyklu + dawkowanie + 0)
+        V_M = 5;
+    elseif(t >= 2*liczba_dni_w_cyklu + 0 && t <= (2*liczba_dni_w_cyklu) + dawkowanie + 0)
+        V_M = 5;
+    elseif(t >= 3*liczba_dni_w_cyklu + 0 && t <= (3*liczba_dni_w_cyklu) + dawkowanie + 0)
+        V_M = 5;
+    elseif(t >= 4*liczba_dni_w_cyklu + 0 && t <= (4*liczba_dni_w_cyklu) + dawkowanie + 0)
+        V_M = 5;
+    elseif(t >= 5*liczba_dni_w_cyklu + 0 && t <= (5*liczba_dni_w_cyklu) + dawkowanie + 0)
+        V_M = 5;
+    elseif(t >= 6*liczba_dni_w_cyklu + 0 && t <= (6*liczba_dni_w_cyklu) + dawkowanie + 0)
+        V_M = 5;
+    elseif(t >= 7*liczba_dni_w_cyklu + 0 && t <= (7*liczba_dni_w_cyklu) + dawkowanie + 0)
+        V_M = 5;
+    elseif(t >= 8*liczba_dni_w_cyklu + 0 && t <= (8*liczba_dni_w_cyklu) + dawkowanie + 0)
+        V_M = 5;
     else
         V_M = 0; 
     end
